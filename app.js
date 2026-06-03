95e9e4fabaebee676467e829dbec8a6a52205516
Leonardo <leonardo_3141240263@utd.edu.mx>
2026-06-03 12:04:51 -0600

    console log

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ btnGenerar.addEventListener('click', () => {
 
     // Removemos la clase 'hidden' para que el contenedor sea visible en la pantalla
     resultadoContenedor.classList.remove('hidden');
-    
+    console.log('Token generado')
     // Opcional: Cambiamos el estilo dinámicamente mediante el DOM para dar feedback visual
     tokenOutput.style.backgroundColor = "#e8f8f5"; 
     tokenOutput.style.borderLeftColor = "#2ecc71"; // Cambia a verde al completarse
